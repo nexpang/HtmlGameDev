d20745672a26092f634dce04f6fdc1c4af9bd009
nexpang <rlarudgur123@gmail.com>
2021-04-07 00:16:20 +0900

    점수 localstorage 저장 및 메인메뉴에 출력

--- dodge/js/App.js
+++ dodge/js/App.js
@@ -12,20 +12,48 @@ class App{
 
         this.canvas = document.querySelector(selector);
         this.ctx = this.canvas.getContext("2d");
-
+        
     }
+    mainMenu(){
+        let list = localStorage.getItem("list");
 
+        if(list == null)
+            list = [];
+        else
+            list = JSON.parse(list);
+
+        clearInterval(this.interver);
+        this.ctx.clearRect(0, 0, 600, 400);
+        this.ctx.drawImage(this.backgroundImg, 0, 0, 600, 400);
+
+        this.ctx.fillStyle = "#000";
+        this.ctx.font = "20px Arial";
+        this.ctx.fillText(`점수판`, 400, 35);
+        this.ctx.font = "10px Arial";
+        if (list.length>10){
+            for(let i = 0; i< 10; i++){
+                this.ctx.fillText(`이름 : ${list[i].name}  점수 : ${list[i].score}s  남길말 : ${list[i].msg}`, 400, 50+(i*15));
+            }
+        }else{
+            for(let i = 0; i< list.length; i++){
+                this.ctx.fillText(`이름 : ${list[i].name}  점수 : ${list[i].score}s  남길말 : ${list[i].msg}`, 400, 50+(i*15));
+            }
+        }
+    }
     gameStart(){
-        let interver = setInterval(()=>{
+        this.interver = setInterval(()=>{
             if(this.gameOver){
+
                 this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
                 this.ctx.fillRect(0,0,600,400);
                 this.ctx.fillStyle = 'rgba(255,255,255,1)';
                 this.ctx.font = "80px Arial";
 
                 this.ctx.fillText("Game Over",80,200);
-
-                clearInterval(interver);
+                
+                clearInterval(this.interver);
+                document.querySelector("#popup").classList.add("on");
+                document.getElementById("score").innerHTML = "점수 : "+Math.round(this.time*100)/100.0+"s";
                 return;
             }
             this.update();
