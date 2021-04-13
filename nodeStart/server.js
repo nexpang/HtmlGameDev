683b89a4c29af12cdad9e7ff100303a01d01d916
nexpang <rlarudgur123@gmail.com>
2021-04-14 00:17:34 +0900

    유사 채팅 서버

---
+++ nodeStart/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const socketio = require('socket.io');
+const express = require('express');
+const app = new express();
+const server = http.createServer(app); //미안 서버야...사과할께..
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+const io = socketio(server); //이렇게하면 서버에 소켓이 붙는다.
+//io는 서버의 모든 소켓을 관리하는 객체
+//on은 이벤트를 연결해주는 것으로 addEventListener과 동일
+io.on("connection", socket => {
+    socket.on("chat", data => {
+        let { msg, nickName } = data;
+        io.emit("chat", { sender: socket.id, msg, nickName });
+    });
+});
+
+server.listen(15454, () => {
+    console.log("서버가 15454포트에서 돌아가고 있습니다.");
+});
